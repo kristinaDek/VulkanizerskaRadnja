57946861daa4629bf268932f244dc55ae279dd10
kristinaDek <kristina.dekic8@gmail.com>
2018-03-27 20:57:08 +0200

    Ispravljene namerne greske unutar klase AutoGuma.

--- VulkanizerskaRadnja/src/gume/AutoGuma.java
+++ VulkanizerskaRadnja/src/gume/AutoGuma.java
@@ -59,7 +59,7 @@ package gume;
 		 * @throws java.lang.RuntimeException ako je parametar manji od 13 i veci od 22
 		 */
 		public void setPrecnik(int precnik) {
-			if (precnik < 13 && precnik > 22)
+			if (precnik < 13 || precnik > 22)
 				throw new RuntimeException("Precnik van opsega");
 			this.precnik = precnik;
 		}
@@ -78,7 +78,7 @@ package gume;
 		 * @throws java.lang.RuntimeException ako je parametar manji od 135 i veci od 355
 		 */
 		public void setSirina(int sirina) {
-			if (sirina < 135 && sirina > 355)
+			if (sirina < 135 || sirina > 355)
 				throw new RuntimeException("Sirina van opsega");
 			this.sirina = sirina;
 		}
